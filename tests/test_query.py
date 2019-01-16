94d5afd04bee337ba6e87eb7a94205cd28db6446
weak_ptr <weak_ptr@163.com>
2019-01-16 12:41:18 +0800

    :white_check_mark: Add test for includes
    Add a testcase for query.includes method. #2

--- tests/test_query.py
+++ tests/test_query.py
@@ -11,74 +11,81 @@ class TestModelQuery(TestCase):
 
     def setUp(self):
         setup()
-        self.cls_name = 'TestModel'
 
-        class ModelA(models.Model):
-            __lc_cls__ = self.cls_name
+        class People(models.Model):
+            __lc_cls__ = 'TEST_People'
 
             name = fields.Field()
             age = fields.Field()
+            profile = fields.Field()
 
-        self.Model = ModelA
+        class PeopleProfile(models.Model):
+            __lc_cls__ = 'TEST_PeopleProfile'
+
+            score = fields.Field()
+
+        self.People = People
+        self.PeopleProfile = PeopleProfile
 
         self.models = [
-            self.Model.create(name='Hi', age=18),
-            self.Model.create(name='Ho', age=21),
-            self.Model.create(name='He', age=23),
+            self.People.create(name='Hi', age=18),
+            self.People.create(name='Ho', age=21),
+            self.People.create(name='He', age=23),
         ]
         for model in self.models:
             model.commit()
 
     def tearDown(self):
-        while True:
-            result = leancloud.Query(self.cls_name).find()
-            if len(result) == 0:
-                break
-            leancloud.Object.destroy_all(result)
+        for clsname in ('TEST_People', 'TEST_PeopleProfile'):
+            while True:
+                result = leancloud.Query(clsname).find()
+                if len(result) == 0:
+                    break
+                leancloud.Object.destroy_all(result)
 
     def test_query_by_equation(self):
-        model = self.Model.query().filter_by(name='Hi').first()
+        model = self.People.query().filter_by(name='Hi').first()
         self.assertEqual(model.name, 'Hi')
         self.assertEqual(model.age, 18)
 
     def test_query_by_less_than(self):
-        model = self.Model.query().filter(self.Model.age < 19).first()
+        model = self.People.query().filter(self.People.age < 19).first()
         self.assertEqual(model.name, 'Hi')
         self.assertEqual(model.age, 18)
 
     def test_query_by_less_than_or_equal_to(self):
-        model = self.Model.query().filter(self.Model.age <= 18).first()
+        model = self.People.query().filter(self.People.age <= 18).first()
         self.assertEqual(model.name, 'Hi')
         self.assertEqual(model.age, 18)
 
-        results = self.Model.query().filter(self.Model.age <= 21).find()
+        results = self.People.query().filter(self.People.age <= 21).find()
         self.assertEqual(len(results), 2)
         for result in results:
             self.assertLessEqual(result.age, 21)
 
     def test_query_by_greater_than(self):
-        model = self.Model.query().filter(self.Model.age > 22).first()
+        model = self.People.query().filter(self.People.age > 22).first()
         self.assertEqual(model.name, 'He')
         self.assertEqual(model.age, 23)
 
     def test_query_by_greater_than_or_equal_to(self):
-        model = self.Model.query().filter(self.Model.age >= 23).first()
+        model = self.People.query().filter(self.People.age >= 23).first()
         self.assertEqual(model.name, 'He')
         self.assertEqual(model.age, 23)
 
-        results = self.Model.query().filter(self.Model.age >= 21).find()
+        results = self.People.query().filter(self.People.age >= 21).find()
         self.assertEqual(len(results), 2)
         for result in results:
             self.assertGreaterEqual(result.age, 21)
 
     def test_query_by_less_and_greater(self):
-        result = self.Model.query().filter(self.Model.age > 18).and_().filter(self.Model.age < 23).find()
+        result = self.People.query().filter(self.People.age > 18).and_().filter(self.People.age < 23).find()
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0].age, 21)
         self.assertEqual(result[0].name, 'Ho')
 
     def test_query_by_less_or_greater(self):
-        result = self.Model.query().filter(self.Model.age < 21).or_().filter(self.Model.age > 21).find()
+        result = self.People.query().filter(self.People.age < 21).or_().filter(self.People.age > 21).find()
         self.assertEqual(len(result), 2)
         result = sorted(result, key=lambda model: model.age)
         self.assertEqual(result[0].age, 18)
@@ -87,64 +94,81 @@ class TestModelQuery(TestCase):
         self.assertEqual(result[1].name, 'He')
 
     def test_query_by_not_equal(self):
-        result = self.Model.query().filter(self.Model.age != 21).find()
+        result = self.People.query().filter(self.People.age != 21).find()
         self.assertEqual(len(result), 2)
 
     def test_query_single_field_multi_condition(self):
-        result = self.Model.query().filter(self.Model.age != 21, self.Model.age != 18).find()
+        result = self.People.query().filter(self.People.age != 21, self.People.age != 18).find()
         self.assertEqual(len(result), 1)
 
     def test_query_by_contains(self):
-        model = self.Model.query().filter(self.Model.name.contains('i')).first()
+        model = self.People.query().filter(self.People.name.contains('i')).first()
         self.assertEqual(model.name, 'Hi')
-        model = self.Model.query().filter(self.Model.name.contains('o')).first()
+        model = self.People.query().filter(self.People.name.contains('o')).first()
         self.assertEqual(model.name, 'Ho')
-        model = self.Model.query().filter(self.Model.name.contains('e')).first()
+        model = self.People.query().filter(self.People.name.contains('e')).first()
         self.assertEqual(model.name, 'He')
 
-        self.assertEqual(len(self.Model.query().filter(self.Model.name.contains('i')).find()), 1)
-        self.assertEqual(len(self.Model.query().filter(self.Model.name.contains('o')).find()), 1)
-        self.assertEqual(len(self.Model.query().filter(self.Model.name.contains('e')).find()), 1)
+        self.assertEqual(len(self.People.query().filter(self.People.name.contains('i')).find()), 1)
+        self.assertEqual(len(self.People.query().filter(self.People.name.contains('o')).find()), 1)
+        self.assertEqual(len(self.People.query().filter(self.People.name.contains('e')).find()), 1)
 
     def test_no_conditions(self):
-        results = self.Model.query().find()
+        results = self.People.query().find()
         self.assertEqual(len(results), 3)
 
     def test_count(self):
-        self.assertEqual(self.Model.query().count(), 3)
+        self.assertEqual(self.People.query().count(), 3)
 
     def test_sort_result_order_by_ascending(self):
-        model = self.Model.query().order_by(self.Model.age.asc).first()
+        model = self.People.query().order_by(self.People.age.asc).first()
         self.assertEqual(model.age, 18)
         self.assertEqual(model.name, 'Hi')
 
     def test_sort_result_order_by_descending(self):
-        model = self.Model.query().order_by(self.Model.age.desc).first()
+        model = self.People.query().order_by(self.People.age.desc).first()
         self.assertEqual(model.age, 23)
         self.assertEqual(model.name, 'He')
 
     def test_condition_in(self):
-        model = self.Model.query().filter(self.Model.name.in_(['Hi', 'hi', 'HI'])).first()
+        model = self.People.query().filter(self.People.name.in_(['Hi', 'hi', 'HI'])).first()
         self.assertEqual(model.name, 'Hi')
         self.assertEqual(model.age, 18)
 
-        model = self.Model.query().filter(self.Model.name.in_(['Hii', 'hii', 'HII'])).first()
+        model = self.People.query().filter(self.People.name.in_(['Hii', 'hii', 'HII'])).first()
         self.assertIsNone(model)
 
     def test_startswith_condition(self):
-        model = self.Model.create(name='iH', age=81)
+        model = self.People.create(name='iH', age=81)
         model.commit()
 
-        results = self.Model.query().filter(self.Model.name.startswith('H')).find()
+        results = self.People.query().filter(self.People.name.startswith('H')).find()
         for result in results:
             self.assertIn(result.name, ('Hi', 'Ho', 'He'))
 
     def test_regex_condition(self):
-        model = self.Model.create(name='iH', age=81)
+        model = self.People.create(name='iH', age=81)
         model.commit()
 
-        results = self.Model.query().filter(self.Model.name.regex('i$')).find()
+        results = self.People.query().filter(self.People.name.regex('i$')).find()
         self.assertEqual(len(results), 1)
         for result in results:
             self.assertEqual(result.name, 'Hi')
             self.assertEqual(result.age, 18)
+
+    def test_query_with_includes(self):
+        profile = self.PeopleProfile.create(score=100)
+        profile.commit()
+
+        people = self.People.query().filter_by(name='Hi').first()
+        people.profile = profile.lc_object
+        people.commit()
+
+        result = self.People.query().filter_by(object_id=people.object_id).includes(self.People.profile).first()
+        self.assertEqual(result.profile.get('score'), 100)
+
+        people = self.People.query().filter_by(name='Ho').includes(self.People.profile).first()
+        self.assertIsNone(people.profile)
+
+        people = self.People.query().filter_by(name='Hi').first()
+        self.assertIsNone(people.profile.get('score'))
