0d04a1a9fdabfb2980c10c0024160346111b566d
weak_ptr <weak_ptr@163.com>
2019-04-05 23:57:37 +0800

    :memo: (storage.models) 为代码添加文档和例子

--- leancloud_better_storage/storage/models.py
+++ leancloud_better_storage/storage/models.py
@@ -35,10 +35,7 @@ model_registry = {}
 
 class ModelMeta(type):
     """
-    ModelMeta
-
-    metaclass of all lean cloud storage models.
-    it fill field property, collect model information and make more function work.
+    Model 元类，收集类字段相关信息，完成初始化默认字段名和注册必须的回调。
     """
     _fields_key = '__fields__'
     _lc_cls_key = '__lc_cls__'
@@ -84,6 +81,31 @@ class ModelMeta(type):
 
 
 class Model(object, metaclass=ModelMeta):
+    """
+    Model 基类，自定义的表模型应当继承于 Model。
+    继承的 Model 类声明方式类似于 SQLAlchemy 和 Django ORM。
+
+    例子:
+    ::
+
+        from leancloud_better_storage.storage.models import Model
+        from leancloud_better_storage.storage.fields import StringField, NumberField
+
+        class Person(Model):
+            name = StringField(max_length=8)
+            age = NumberField()
+
+    模型已经包含了三个由 LeanCloud 产生的字段：
+
+    - `object_id`
+    - `created_at`
+    - `updated_at`
+
+    这三个字段会由 LeanCloud 自动填充，所以无需在调用`create`时传入。尤其是对于 `object_id`，
+    通常不应在创建时指定。
+
+    关于可以使用的字段类，请参考 :ref:`leancloud_better_storage.storage.fields`。
+    """
     __lc_cls__ = ''
     __fields__ = {}  # type: dict
 
@@ -124,22 +146,74 @@ class Model(object, metaclass=ModelMeta):
 
     @classmethod
     def create(cls, **kwargs):
+        """
+        创建一个 Model 实例。这个实例不会立即保存至 LeanCloud。
+
+        你可以通过关键字参数来初始化实例字段。
+
+        看下面的例子： ::
+
+            from leancloud_better_storage.storage.models import Model
+            from leancloud_better_storage.storage.fields import StringField, NumberField
+
+            class Person(Model):
+                name = StringField(nullable=False)
+                age = NumberField()
+
+            remilia = Person.create(name='remilia scarlet', age=549)  # just a random age number, ignore it.
+            flandre = Person.create(name='flandre scarlet', age=549)
+
+        被指定 `nullable` 为 `False` 并且 `default` 为 `None` 、 `undefined` 的字段必须在初始化时提供，否则将会产生一个 `KeyError` 。
+
+        需要注意的是 `default` 被指定为 `None` 时的行为和 `undefined` 行为不同，因为在传输和存储的过程，
+        序列化为JSON/BSON的Python的 `None` 会被认作 `null` ，而 `undefined` 则令字段不出现。
+        这一概念与 JavaScript 近乎一致。
+
+        :param kwargs: 初始化字段
+        :return:
+        """
         _validate(cls.__fields__, kwargs)
         return cls(leancloud.Object.create(cls.__lc_cls__, **_merge_default_and_args(cls.__fields__, kwargs)))
 
     def commit(self):
+        """
+        保存这个对象到 LeanCloud。
+
+        使用 LeanCloud SDK 的 `save` 实现。
+
+        会触发 `pre_create` 回调。
+
+        :return: self
+        """
         self._do_life_cycle_hook('pre_create' if self.object_id is None else 'pre_update')
         self._lc_obj.save()
         return self
 
     @classmethod
     def commit_all(cls, *models):
+        """
+        保存多个对象到 LeanCloud。所有传入的对象必须是同一个 Model 类的实例。
+
+        使用 LeanCloud SDK 的 `save_all` 实现。
+
+        保存会触发 `pre_create` 回调。
+
+        :param models:
+        :return:
+        """
         for instance in models:
             instance._do_life_cycle_hook('pre_create' if instance.object_id is None else 'pre_update')
 
         leancloud.Object.extend(cls.__lc_cls__).save_all([instance._lc_obj for instance in models])
 
     def drop(self):
+        """
+        从 LeanCloud 删除这个对象。
+
+        删除后对象进入不可用状态，请不要再做任何读写操作。
+
+        :return: None
+        """
         self._do_life_cycle_hook('pre_delete')
 
         self._lc_obj.destroy()
@@ -147,6 +221,16 @@ class Model(object, metaclass=ModelMeta):
 
     @classmethod
     def drop_all(cls, *models):
+        """
+        从 LeanCloud 删除多个对象，所有传入的对象必须是同一个 Model 的实例。
+
+        删除使用 LeanCloud SDK 提供的 `destroy_all` 实现。
+
+        删除会触发在 Model 类注册的 `pre_delete` 回调。
+
+        :param models: 一个或多个 Model 类实例
+        :return: None
+        """
         for instance in models:
             instance._do_life_cycle_hook('pre_delete')
 
